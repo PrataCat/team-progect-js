723842c82883cc93a070c49f11b16c186c03a0e6
VladZhyrnyi <vladzhyrnyi@ukr.net>
2023-03-02 19:52:52 +0200

    add some styles and js

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,23 @@
 //------Vlad-------
+
+refs = {
+  header: document.querySelector('.header'),
+  searchForm: document.querySelector('.search-form'),
+  searchBtn: document.querySelector('.search-form__btn'),
+  menuBtn: document.querySelector('.menu-btn'),
+  menu: document.querySelector('.mobile-menu'),
+};
+
+refs.menuBtn.addEventListener('click', onMenuBtnClick);
+refs.searchBtn.addEventListener('click', onSearchButtonClick);
+
+function onMenuBtnClick(event) {
+  event.currentTarget.classList.toggle('isActive');
+  // refs.header.classList.toggle();
+  refs.menu.classList.toggle('isActive');
+}
+
+function onSearchButtonClick(event) {
+  console.log('qqq');
+  refs.searchForm.classList.add('isShown');
+}
