a5a5f2836bb69e0d41bf9e6d010889af1a5fbaf4
ZhuravlevAlexei <110524860+ZhuravlevAlexei@users.noreply.github.com>
2023-03-05 10:43:19 +0200

    Update favorite.js

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -60,53 +60,6 @@ function createPopularNewsCollection(arr) {
   const arrForMarkup = displayItems(arrCurrentNews, (page = 1), perPage); // массив для рендера на текущую страницу
   const cardMarkupArray = arrForMarkup.map(el => creatCardMarkup(el)); // массив готовой разметки карточек для рендера на текущую страницу
 
-  // const markupNewsCollection = arr
-  //   .map(el => {
-  //     const { abstract, title, url, published_date, media, section, id } = el;
-  //     // const foto = media[0]['media-metadata'][2].url;
-  //     const foto = media[0] ? media[0]['media-metadata'][2].url : '';
-  //     const data = published_date.split('-').reverse().join('/');
-  //     return `<li class="box-news__item" data-id="${id}">
-  //     <p class="box-news__section">${section}</p>
-  //         <div class="box-news___wrap-reading">
-  //           <span class="box-news__reading">Already read</span>
-  //           <svg class="icon" width="18" height="18">
-  //             <use href="#icon-check"></use>
-  //           </svg>
-  //         </div>
-  //         <article>
-  //           <div class="box-news__thumb">
-  //             <img class="box-news__img" src="${foto}" loading="lazy" alt="${section}" width='440'/>
-  //             <button class="box-news__btn" id="${id}">Remove from favorite
-  //               <svg class="box-news__btn-icon" width="16" height="16" id="${id}">
-  //                 <use href="../images/akar-icons_heart-filled.svg"></use>
-  //               </svg>
-  //             </button>
-  //           </div>
-  //         <div class="box-news__wrap-title">
-  //           <h2 class="box-news__titel">
-  //             ${title}
-  //           </h2>
-  //         </div>
-  //         <div class="box-news__wrap-text">
-  //           <p class="box-news__text">
-  //             ${
-  //               abstract.length > 120
-  //                 ? abstract.slice(0, 110) + '...'
-  //                 : abstract
-  //             }
-  //           </p>
-  //         </div>
-  //         <div class="box-news__wrap">
-  //           <p class="box-news__data">${data}</p>
-  //           <a href="${url}" id="${id}" class="box-news__link" target="_blank" rel="noopener noreferrer nofollow">Read more</a>
-  //         </div>
-  //         </article>
-  //         <div class="overlay"></div>
-  //       </li>`;
-  //   })
-  //   .join('');
-
   box.insertAdjacentHTML('beforeend', cardMarkupArray.join(''));
 }
 
