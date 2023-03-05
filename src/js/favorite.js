1edb38d8659e61260821527c3a170ad10e763c20
ZhuravlevAlexei <110524860+ZhuravlevAlexei@users.noreply.github.com>
2023-03-05 10:31:15 +0200

    markup

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -1,20 +1,7 @@
 //------------Oleksiy Zhuravlev-----------
 
 import { excludeFavoriteNew, loadAllFavorites } from './library.js';
-
-// const doubleButton = document.querySelector('.double-first-new');
-// doubleButton.addEventListener('click', onDouble);
-
-// function onDouble(params) {
-//   const arr = loadAllFavorites();
-
-//   const newToDuble = arr.slice(0, 1);
-
-//   // console.log(newToDuble[0]);
-//   includeFavoriteNew(newToDuble[0]);
-//   box.innerHTML = '';
-//   createPopularNewsCollection(loadAllFavorites());
-// }
+import { creatCardMarkup } from './creatCardMarkup';
 
 const box = document.querySelector('.box-news');
 
@@ -35,6 +22,31 @@ function onClick(e) {
   }
 }
 
+// ф-ци возвращае ширину вью порта
+function checkWidth() {
+  return document.documentElement.clientWidth;
+}
+
+function cardsPerPage() {
+  const width = checkWidth();
+
+  if (width > 1280) {
+    return 8;
+  } else if (width > 768) {
+    return 7;
+  } else {
+    return 4;
+  }
+}
+
+// ф-ция возвращает массив карточек для рендера на текущую страницу (из текущуго массива arrCurrentNews)
+function displayItems(arr, page, perPage) {
+  const start = (page - 1) * perPage;
+  const end = start + perPage;
+  const paginatedEl = arr.slice(start, end);
+  return paginatedEl;
+}
+
 function createPopularNewsCollection(arr) {
   if (typeof arr !== 'object') {
     return;
@@ -42,54 +54,60 @@ function createPopularNewsCollection(arr) {
   if (Array.isArray(arr) === false) {
     return;
   }
-  const markupNewsCollection = arr
-    .map(el => {
-      const { abstract, title, url, published_date, media, section, id } = el;
-      // const foto = media[0]['media-metadata'][2].url;
-      const foto = media[0] ? media[0]['media-metadata'][2].url : '';
-      const data = published_date.split('-').reverse().join('/');
-      return `<li class="box-news__item" data-id="${id}">
-      <p class="box-news__section">${section}</p>
-          <div class="box-news___wrap-reading">
-            <span class="box-news__reading">Already read</span>
-            <svg class="icon" width="18" height="18">
-              <use href="#icon-check"></use>
-            </svg> 
-          </div>
-          <article>
-            <div class="box-news__thumb">
-              <img class="box-news__img" src="${foto}" loading="lazy" alt="${section}" width='440'/>
-              <button class="box-news__btn" id="${id}">Remove from favorite
-                <svg class="box-news__btn-icon" width="16" height="16" id="${id}">
-                  <use href="../images/akar-icons_heart-filled.svg"></use>
-                </svg>
-              </button>
-            </div>
-          <div class="box-news__wrap-title">
-            <h2 class="box-news__titel">
-              ${title}
-            </h2>
-          </div>
-          <div class="box-news__wrap-text">
-            <p class="box-news__text">
-              ${
-                abstract.length > 120
-                  ? abstract.slice(0, 110) + '...'
-                  : abstract
-              }
-            </p>
-          </div>
-          <div class="box-news__wrap">
-            <p class="box-news__data">${data}</p>
-            <a href="${url}" id="${id}" class="box-news__link" target="_blank" rel="noopener noreferrer nofollow">Read more</a>
-          </div>
-          </article>
-          <div class="overlay"></div>
-        </li>`;
-    })
-    .join('');
 
-  box.insertAdjacentHTML('beforeend', markupNewsCollection);
+  perPage = cardsPerPage(); // замеряем ширину вью порта
+  const arrCurrentNews = loadAllFavorites();
+  const arrForMarkup = displayItems(arrCurrentNews, (page = 1), perPage); // массив для рендера на текущую страницу
+  const cardMarkupArray = arrForMarkup.map(el => creatCardMarkup(el)); // массив готовой разметки карточек для рендера на текущую страницу
+
+  // const markupNewsCollection = arr
+  //   .map(el => {
+  //     const { abstract, title, url, published_date, media, section, id } = el;
+  //     // const foto = media[0]['media-metadata'][2].url;
+  //     const foto = media[0] ? media[0]['media-metadata'][2].url : '';
+  //     const data = published_date.split('-').reverse().join('/');
+  //     return `<li class="box-news__item" data-id="${id}">
+  //     <p class="box-news__section">${section}</p>
+  //         <div class="box-news___wrap-reading">
+  //           <span class="box-news__reading">Already read</span>
+  //           <svg class="icon" width="18" height="18">
+  //             <use href="#icon-check"></use>
+  //           </svg>
+  //         </div>
+  //         <article>
+  //           <div class="box-news__thumb">
+  //             <img class="box-news__img" src="${foto}" loading="lazy" alt="${section}" width='440'/>
+  //             <button class="box-news__btn" id="${id}">Remove from favorite
+  //               <svg class="box-news__btn-icon" width="16" height="16" id="${id}">
+  //                 <use href="../images/akar-icons_heart-filled.svg"></use>
+  //               </svg>
+  //             </button>
+  //           </div>
+  //         <div class="box-news__wrap-title">
+  //           <h2 class="box-news__titel">
+  //             ${title}
+  //           </h2>
+  //         </div>
+  //         <div class="box-news__wrap-text">
+  //           <p class="box-news__text">
+  //             ${
+  //               abstract.length > 120
+  //                 ? abstract.slice(0, 110) + '...'
+  //                 : abstract
+  //             }
+  //           </p>
+  //         </div>
+  //         <div class="box-news__wrap">
+  //           <p class="box-news__data">${data}</p>
+  //           <a href="${url}" id="${id}" class="box-news__link" target="_blank" rel="noopener noreferrer nofollow">Read more</a>
+  //         </div>
+  //         </article>
+  //         <div class="overlay"></div>
+  //       </li>`;
+  //   })
+  //   .join('');
+
+  box.insertAdjacentHTML('beforeend', cardMarkupArray.join(''));
 }
 
 createPopularNewsCollection(loadAllFavorites());
