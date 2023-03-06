777f8f1b024ef0cefda9e03427a6c455f6e753a0
Oleksii Korolenko <as_vostok@i.ua>
2023-03-06 15:18:00 +0200

    add ttt

--- src/js/actions-with-flags.js
+++ src/js/actions-with-flags.js
@@ -2,17 +2,11 @@ import { sendCurrentArray as getArray } from './news-list';
 import { loadAllFavorites } from './library';
 import { loadReadStorage } from './read-library';
 
-// const arr = loadReadStorage();
-// console.log(arr);
-
 export async function setFlags() {
-  let arrCurrent = await getArray();
-  let arrFav = await loadAllFavorites();
-  let objRead = await loadReadStorage();
-  let arrRead = Object.values(objRead)[0];
-  console.log('setFlags  arrRead:', objRead);
-
-  // console.log(arrFav === false);
+  let arrCurrent = getArray();
+  let arrFav = loadAllFavorites();
+  let objRead = loadReadStorage();
+  let arrRead = Object.values(objRead).flat();
 
   arrCurrent.map(el => {
     if (arrFav) {
@@ -31,22 +25,6 @@ export async function setFlags() {
       }
     }
   });
-  console.log('Это лог из action --- ', arrCurrent);
   return arrCurrent;
 }
 
-// можно использовать тернарник
-// setFlags();
-
-// const answer = arrFav.some(item => el.id === item.id);
-// if (answer) {
-//   el.favor = true;
-// } else {
-//   el.favor;
-// }
-// const reply = arrRead.some(i => el.id === i.id);
-// if (reply) {
-//   el.read = true;
-// } else {
-//   el.read;
-// }
